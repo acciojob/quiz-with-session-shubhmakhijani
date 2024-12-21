26aa5f8853a8fcea3ba47b44cfca8d3bbe9a7121
shubhmakhijani <124859260+shubhmakhijani@users.noreply.github.com>
2024-12-22 04:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -53,8 +53,6 @@ function submitQuiz() {
 
 questionsElement.addEventListener('change', saveProgress);
 
-renderQuestions();
-
 const questions = [
   {
     question: "What is the capital of France?",
@@ -83,27 +81,4 @@ const questions = [
   },
 ];
 
-function renderQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
-      questionElement.appendChild(document.createElement("br"));
-    }
-    questionsElement.appendChild(questionElement);
-  }
-}
 renderQuestions();
